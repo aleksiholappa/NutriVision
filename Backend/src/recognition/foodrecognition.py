835a24130935da53803d1cf8bc1b1aaafcc566fa
LOKK11 <myllymakiossi@gmail.com>
2025-03-20 13:50:39 +0200

    Update food recognition endpoint

--- Backend/src/recognition/foodrecognition.py
+++ Backend/src/recognition/foodrecognition.py
@@ -4,55 +4,67 @@ from clarifai_grpc.grpc.api.status import status_code_pb2
 import csv
 
 PAT = ""
-USER_ID = "holappa" 
+USER_ID = "holappa"
 APP_ID = "food_recognition"
 MODEL_ID = "food-item-v1-recognition"
-IMAGE_PATH = "" 
 
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
 
-metadata = (("authorization", f"Key {PAT}"),)
+def initialize_model(image_bytes):
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
 
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+    metadata = (("authorization", f"Key {PAT}"),)
 
-with open(IMAGE_PATH, "rb") as f:
-    image_bytes = f.read()
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
-post_model_outputs_response = stub.PostModelOutputs(
-    service_pb2.PostModelOutputsRequest(
-        user_app_id=userDataObject,
-        model_id=MODEL_ID,
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    image=resources_pb2.Image(
-                        base64=image_bytes
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            user_app_id=userDataObject,
+            model_id=MODEL_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        image=resources_pb2.Image(base64=image_bytes)
                     )
                 )
-            )
-        ]
-    ),
-    metadata=metadata
-)
-if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
-    print(post_model_outputs_response.status)
-    raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+            ],
+        ),
+        metadata=metadata,
+    )
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        print(post_model_outputs_response.status)
+        raise Exception(
+            "Post model outputs failed, status: "
+            + post_model_outputs_response.status.description
+        )
+    return post_model_outputs_response
+
 
 def get_food_id(food_name, csv_file):
-    with open(csv_file, mode='r', encoding='ISO-8859-1') as file:
-        reader = csv.DictReader(file, delimiter=';')
+    with open(csv_file, mode="r", encoding="ISO-8859-1") as file:
+        reader = csv.DictReader(file, delimiter=";")
         for row in reader:
             food = row["FOODNAME"].split(",")[0].strip().lower()
             if food == food_name.strip().lower():
                 return row["FOODID"]
     return None
 
+
 def get_macronutrients(food_id, csv_file):
-    macronutrients = {"Kilocalories": None, "Protein": None, "Carbohydrates": None, "Fat": None}
-    eufdname = {"ENERC": "Kilocalories", "PROT": "Protein", "CHOAVL": "Carbohydrates", "FAT": "Fat"}
-    with open(csv_file, mode='r', encoding='ISO-8859-1') as file:
-        reader = csv.reader(file, delimiter=';')
+    macronutrients = {
+        "Kilocalories": None,
+        "Protein": None,
+        "Carbohydrates": None,
+        "Fat": None,
+    }
+    eufdname = {
+        "ENERC": "Kilocalories",
+        "PROT": "Protein",
+        "CHOAVL": "Carbohydrates",
+        "FAT": "Fat",
+    }
+    with open(csv_file, mode="r", encoding="ISO-8859-1") as file:
+        reader = csv.reader(file, delimiter=";")
         for row in reader:
             if row[0] == str(food_id) and row[1] in eufdname:
                 value = float(row[2].replace(",", "."))
@@ -61,20 +73,32 @@ def get_macronutrients(food_id, csv_file):
                 macronutrients[eufdname[row[1]]] = value
     return macronutrients
 
-for output in post_model_outputs_response.outputs:
-    if hasattr(output.data, "concepts") and output.data.concepts:
-        print("Detected food items:")
-        for concept in output.data.concepts:
-            if concept.value > 0.75:  # Confidence threshold
-                print(f"\n{concept.name}, {concept.value:.4f}")
-                food_id = get_food_id(concept.name, "foodname_EN.csv")
-                if food_id != None:
-                    macronutrients = get_macronutrients(food_id, "component_value.csv")
-                    print(f"Macronutrients for {concept.name} / 100 grams:")
-                    for key, value in macronutrients.items():
-                        print(f"{key}: {value}")
-                else:
-                    print(f"Food item {concept.name} not found in database.")
-    else:
-        print("No food items detected in output.")
-        
+
+def detect_food_items(post_model_outputs_response):
+    items = []
+    for output in post_model_outputs_response.outputs:
+        if hasattr(output.data, "concepts") and output.data.concepts:
+            for concept in output.data.concepts:
+                if concept.value > 0.75:  # Confidence threshold
+                    food_id = get_food_id(concept.name, "foodname_EN.csv")
+                    if food_id:
+                        macronutrients = get_macronutrients(
+                            food_id, "component_value.csv"
+                        )
+                        document = {
+                            "name": concept.name,
+                            "confidence": concept.value,
+                            "macronutrients": macronutrients,
+                        }
+                        items.append(document)
+        else:
+            raise Exception("No food items detected in output.")
+
+    return items
+
+
+def recognize_image(file_path):
+    with open(file_path, "rb") as f:
+        image_bytes = f.read()
+    post_model_outputs_response = initialize_model(image_bytes)
+    return detect_food_items(post_model_outputs_response)
--- Backend/src/recognition/main.py
+++ Backend/src/recognition/main.py
@@ -1,26 +1,61 @@
+import json
+import os
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from typing import Optional
+import logging
+import sys
+import foodrecognition as fr
+
+# Configure the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # Set the desired logging level
+
+# Create a console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)  # Set the handler's logging level
+
+# Create a formatter
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
 
 app = FastAPI()
 
-@app.post("/imgApi/recognize/")
-async def upload_image(image: UploadFile = File(..., description="The image to process"),
-                       description: Optional[str] = Form(None, description="Optional description of the image")):
+UPLOAD_DIR = "./uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.post("/recognize")
+async def recognize_image(
+    image: UploadFile = File(..., description="The image to process"),
+    description: Optional[str] = Form(
+        None, description="Optional description of the image"
+    ),
+):
     """
-    Endpoint to receive an image and optional description.
+    Endpoint to receive an image and use the food recognition
+    model to identify the food in the image.
     """
     try:
-        
         contents = await image.read()
-        print(f"Contents of {image.filename}: {contents}")
 
-        
-        file_path = f"uploads/{image.filename}"
-        with open(image.filename, "wb") as f:
+        # Save the image to the uploads directory
+        file_path = os.path.join(UPLOAD_DIR, image.filename)
+        with open(file_path, "wb") as f:
             f.write(contents)
-        print(f"Image saved to {file_path}")
-        return {"filename": image.filename, "description": description, "message": "Image uploaded successfully"}
+        logger.info(f"Saved image to {file_path}")
+
+        # Call the food recognition model
+        result = fr.recognize_image(file_path)
+        logger.info(f"Food recognition result:\n{json.dumps(result)}")
+
+        # Remove the image file
+        os.remove(file_path)
+
+        # Return the result
+        return result
 
     except Exception as e:
         return {"error": str(e)}
-    
