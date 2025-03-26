346929a0623afece706d1d0cad0e122e83052233
Aleksi <alholapp22@student.oulu.fi>
2025-03-26 13:09:21 +0200

    update food recognition capabilities

--- Backend/src/recognition/foodrecognition.py
+++ Backend/src/recognition/foodrecognition.py
@@ -2,15 +2,15 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 import csv
+from rapidfuzz import fuzz, process
 import dotenv
 import os
 
 dotenv.load_dotenv()
-
 PAT = os.getenv("PAT", "")
-USER_ID = "holappa"
-APP_ID = "food_recognition"
-MODEL_ID = "food-item-v1-recognition"
+USER_ID = os.getenv("USER_ID", "")
+APP_ID = os.getenv("APP_ID", "")
+MODEL_ID = os.getenv("MODEL_ID", "")
 
 
 def initialize_model(image_bytes):
@@ -45,12 +45,23 @@ def initialize_model(image_bytes):
 
 
 def get_food_id(food_name, csv_file):
-    with open(csv_file, mode="r", encoding="ISO-8859-1") as file:
-        reader = csv.DictReader(file, delimiter=";")
+    with open(csv_file, mode='r', encoding='ISO-8859-1') as file:
+        matches = {}
+        fuzzwords = {}
+        reader = csv.DictReader(file, delimiter=';')
         for row in reader:
             food = row["FOODNAME"].split(",")[0].strip().lower()
+            food_full = row["FOODNAME"].strip().lower()
             if food == food_name.strip().lower():
                 return row["FOODID"]
+            if food_name.strip().lower() in food_full:
+                matches[food_full] = row["FOODID"]
+            fuzzwords[food] = row["FOODID"]
+        if matches:
+            return list(matches.values())[0]
+        similarities = process.extractOne(food_name, fuzzwords.keys(), scorer=fuzz.ratio)
+        if similarities[1] > 80:
+            return fuzzwords[similarities[0]]
     return None
 
 
@@ -73,7 +84,7 @@ def get_macronutrients(food_id, csv_file):
             if row[0] == str(food_id) and row[1] in eufdname:
                 value = float(row[2].replace(",", "."))
                 if row[1] == "ENERC":
-                    value = round(value * 0.239, 2)
+                    value = round(value * 0.239, 2) # kJ to kcal
                 macronutrients[eufdname[row[1]]] = value
     return macronutrients
 
@@ -91,10 +102,15 @@ def detect_food_items(post_model_outputs_response):
                         )
                         document = {
                             "name": concept.name,
-                            "confidence": concept.value,
+                            "confidence": round(concept.value, 4),
                             "macronutrients": macronutrients,
                         }
                         items.append(document)
+                    else:
+                        document = {
+                            "name": concept.name,
+                            "confidence": round(concept.value, 4),
+                        }
         else:
             raise Exception("No food items detected in output.")
 
